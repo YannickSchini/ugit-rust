f5396674e63b8e3006be75077ea8aa21d84410c9
Pablo COVES <pablo.coves@anatoscope.com>
2022-06-21 16:06:20 +0200

    Add argument parsing.
    Add an optional subcommand.
    Defaults to `init`.
    
    Feature: Clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod options;
+
+use crate::options::{Options, Parser, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let options = Options::parse();
+
+    match options.sub_command {
+        Some(SubCommand::Init) => init(),
+        None => init(),
+    }
+}
+
+fn init() {
+    println!("Hello world")
 }
