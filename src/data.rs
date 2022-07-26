5b3f78126769c2ca2c32ebbc811a7d1c919a1380
Yannick Schini <yannick.schini@qonto.com>
2022-07-26 22:04:15 +0200

    feat: Add file storage feature
    See information here:
    https://www.leshenko.net/p/ugit/#hash-object

--- src/data.rs
+++ src/data.rs
@@ -1,8 +1,30 @@
-use std::fs;
+use std::{fs, path::PathBuf};
+use std::fs::File;
+use std::io::prelude::*;
+use crypto::digest::Digest;
+use crypto::sha1::Sha1;
+
+use crate::error::Result;
 
 static UGIT_DIR: &str = ".ugit";
 
-pub fn init() -> std::io::Result<()> {
+pub fn init() -> Result<()> {
     fs::create_dir(UGIT_DIR)?;
+    fs::create_dir(format!("{}/objects", UGIT_DIR))?;
     Ok(())
+}
+
+pub fn hash_file(file_name: PathBuf) -> Result<String> {
+    let mut f = File::open(file_name)?;
+    let mut buffer = String::new();
+    f.read_to_string(&mut buffer)?;
+
+    let mut hasher = Sha1::new();
+    hasher.input_str(&buffer);
+    let sha1_identifier = hasher.result_str();
+
+    let mut output_file = File::create(format!("{}/objects/{}", UGIT_DIR, sha1_identifier))?;
+    write!(&mut output_file, "{}", &buffer)?;
+
+    Ok(sha1_identifier)
 }
